3d3e8162eb1d7666e357f409d4883822c7526fc2
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-09 10:04:35 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 122; a++)
+		putchar(a);
+	for (a = 65; a < 90; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
