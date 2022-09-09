44200711d67320e7991c8b427e992e1922da3bce
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-09 10:07:16 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	int a;
 
-	for (a = 97; a < 122; a++)
+	for (a = 97; a < 123; a++)
 		putchar(a);
-	for (a = 65; a < 90; a++)
+	for (a = 65; a < 91; a++)
 		putchar(a);
 	putchar('\n');
 	return (0);
