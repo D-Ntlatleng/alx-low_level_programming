c2e9a439ed696d5aa35abffbb9ad28547862ab60
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-08 19:14:56 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
