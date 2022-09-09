a83ba2caf69acdfe46767744d4d7c80098e4d76e
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-09 09:47:35 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
