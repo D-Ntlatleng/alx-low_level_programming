7e8f2d42db5fbc3a5d58e891bed642dc2f4463ed
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-11 02:24:31 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  *
  * Description: 'the program's description'
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
