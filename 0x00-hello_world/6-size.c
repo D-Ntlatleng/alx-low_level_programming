e946551a4647ea03b618fa4c468bd6d09230b177
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-11 02:02:39 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,9 +17,9 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(c));
-	printf("Size of a int: %zu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+	printf("Size of a int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(lli));
 	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
