e859e0dedeaaecffcf6e5fa8d6d1d27c8ab2dc3e
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-13 14:38:00 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - check lower case
+ * @c: check if is lower
+ *
+ * Description: 'the program's description'
+ *
+ * Return: 1 (success) and 0 (failed)
+ */
+
+int _abs(int c)
+{
+	return (c >= 0 ? c : c * -1);
+}
