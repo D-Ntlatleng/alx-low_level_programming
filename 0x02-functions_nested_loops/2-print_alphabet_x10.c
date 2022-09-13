d5f4561c429e72cc63dbecf40c956c427371e319
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-13 13:15:47 -0700

    2-print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,21 @@
  * Return: void 
  */
 
+void my_print(void)
+{
+        int i;
+        char str[] = "abcdefghijklmnopqrstuvwxyz";
+
+        i = 0;
+        while (str[i])
+                _putchar(str[i++]);
+        _putchar(10);
+}
+
 void print_alphabet_x10(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		print_alphabet();
+		my_print();
 }
