0cc1065ae597eecd4f1abb0a3a773c487efa0df2
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-13 17:47:04 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - check lower case
+ * @c: int c
+ * @b: int b
+ *
+ * Description: 'the program's description'
+ *
+ * Return: result of c + b
+ */
+
+int add(int c, int b)
+{
+	return (c + b);
+}
