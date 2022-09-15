00ab51a77aa877af7c01dc47d3291d1174e9c8d5
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-15 13:01:29 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Makes the product of two numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: The product of the two parameters
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,5 @@ void positive_or_negative(int);
 int largest_number(int, int, int);
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
 #endif /* MAIN_H */
