ed67f217ed45ca33373450b909c2337e87237302
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-15 12:42:57 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - check is charector is upper case
+ * @c: First operand
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
