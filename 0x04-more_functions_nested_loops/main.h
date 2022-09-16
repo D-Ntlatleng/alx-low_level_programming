4041b3d209df364948df78bb43db5d493cf7dc25
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-16 10:46:42 -0700

    task3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,21 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void positive_or_negative(int);
 int largest_number(int, int, int);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int add(int, int);
+void times_table(void);
+void print_numbers(void);
 #endif /* MAIN_H */
