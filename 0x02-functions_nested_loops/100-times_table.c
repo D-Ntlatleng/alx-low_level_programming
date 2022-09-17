4c35d8d20e0840d13f4840b3fd806385e81c6784
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-17 12:44:59 -0700

    task100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,10 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				printf("%4d", j * i);
+				if (j == 0)
+					printf("%d", j * i);
+				else
+					printf("%4d", j * i);
 				if (j < n)
 					printf(",");
 			}
