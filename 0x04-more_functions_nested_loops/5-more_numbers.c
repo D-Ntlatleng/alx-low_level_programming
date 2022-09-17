ebf04bc77968a6c3c2f3ffbe3f7776f284ba73e6
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-17 04:34:48 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+void my_print(int n);
 /**
  * more_numbers - prints 0 to 14 10X
  *
@@ -13,12 +14,22 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
-			_putchar(j + 48);
+			my_print(j + 48);
 		for (j = 0; j < 5; j++)
 		{
-			_putchar(49);
-			_putchar(j + 48);
+			my_print(49);
+			my_print(j + 48);
 		}
-		_putchar(10);
+		my_print(10);
 	}
+}
+/**
+ * my_print - calling _putchar
+ *@n: integer
+ *
+ * Return: void
+ */
+void my_print(int n)
+{
+	_putchar(n);
 }
