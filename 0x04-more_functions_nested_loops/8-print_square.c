37676771660ae0c2152f73c5bde34ad4841944d5
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-17 04:37:16 -0700

    task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_size - check the code
+ *@size: size is the size of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+	int c;
+
+	c = 0;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar(35);
+		_putchar(10);
+		c++;
+	}
+	if (!c)
+		_putchar(10);
+}
