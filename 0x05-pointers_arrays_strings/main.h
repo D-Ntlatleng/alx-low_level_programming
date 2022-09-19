b11182e98f937d593c2141fe029a7874409de3f2
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-19 09:20:24 -0700

    task6

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -31,5 +31,10 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strycpy(char *dest, char *str);
 
 #endif /* MAIN_H */
