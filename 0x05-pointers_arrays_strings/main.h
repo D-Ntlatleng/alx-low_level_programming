9bed9529b71ebd0ad472468528cfb0875332603d
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-19 08:03:07 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void positive_or_negative(int);
+int largest_number(int, int, int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int add(int, int);
+void times_table(void);
+void print_numbers(void);
+void print_most_numbers(void);
+void print_line(int);
+void more_numbers(void);
+void print_diagonal(int);
+void print_square(int size);
+void more_numbers(void);
+void print_triangle(int size);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
