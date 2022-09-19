52c7b1a6e726e78fd87ba5a46ceece4e430b3894
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-19 10:18:26 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+
+	i = 0;
+	n = 0;
+	while (str[i])
+		i++;
+	if (i % 2 == 0)
+		n = i / 2;
+	else
+		n = (i - 1) / 2;
+	while (str[n])
+		_putchar(str[n++]);
+	_putchar(10);
+}
