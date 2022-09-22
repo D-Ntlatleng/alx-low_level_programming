a2a266f56dd69694d47201f6864cfb834f53af36
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-09-22 08:32:46 -0700

    task101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * print_number - print out number
+ * @n: integer to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	char *s;
+
+	if (n == INT_MAX)
+	{
+		s = "2147483647";
+		while (*s)
+			_putchar(*s++);
+		return;
+	}
+	if (n == INT_MIN)
+	{
+		s = "-2147483648";
+		while (*s)
+			_putchar(*s++);
+		return;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n > 10)
+		print_number(n / 10);
+	_putchar(n % 10 + 48);
+}
