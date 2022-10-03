db5d22c65e6048059d26bdaf38597a03deef4090
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-10-03 11:17:45 -0700

    task2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: char pointer or NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, k;
+	char *str;
+
+	i = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[i])
+		i++;
+	str = malloc(sizeof(char) * i + 1);
+	if (str != NULL)
+	{
+		k = 0;
+		while (*s1)
+			str[k++] = *s1++;
+		while (*s2)
+			str[k++] = *s2++;
+		str[k] = '\0';
+	}
+	return (str);
+}
