3922d7c7f5676c6f19cf39a2477b41e1da9aa422
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-10-03 12:13:11 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,16 @@ int **alloc_grid(int width, int height)
 		if (grid)
 		{
 			for (i = 0; i < height; i++)
+			{
 				grid[i] = (int *)malloc(sizeof(int) * width);
+				if (grid[i] == NULL)
+				{
+					for (j = 0; j < i; j++)
+						free(grid[j]);
+					free(grid);
+					return (NULL);
+				}
+			}
 			for (i = 0; i < height; i++)
 				for (j = 0; j < width; j++)
 					grid[i][j] = 0;
