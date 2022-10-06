78d6b4d54d1a4b8483b5f73e448cdce4b1309ca0
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-10-06 09:58:17 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = NULL;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
