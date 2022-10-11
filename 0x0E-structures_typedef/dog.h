725a7fc33ce1dc1af29da7b31790ac3e5339d04a
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-10-11 06:56:17 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
+/**
+ * struct dog - struct for a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
