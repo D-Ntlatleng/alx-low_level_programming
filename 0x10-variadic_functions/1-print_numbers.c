ea155d097d54576089f6206cb26ad4b49c628cf2
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-10-14 07:48:32 -0700

    task1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers with a separator
+ * @separator: charector to separate numbers
+ * @n: number of numbers to print
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+	if (separator && n)
+	{
+		va_start(ptr, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(ptr, int));
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+	}
+}
