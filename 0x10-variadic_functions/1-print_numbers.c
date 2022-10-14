3be792b68918874bb04a9d41f6a3d289dd4c7319
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-10-14 07:50:37 -0700

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr;
 
-	if (separator && n)
+	if (separator != NULL && n > 0)
 	{
 		va_start(ptr, n);
 		for (i = 0; i < n; i++)
