6e58b93cf44a035a8e74f6f188588157c50e1262
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-11-05 03:59:04 -0700

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function to delete first elememnt in the linked list
+ * @head: list to delete
+ *
+ * Return: 0 or some number
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int n;
+
+	if (head)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		n = ptr->n;
+		free(ptr);
+		return (n);
+	}
+	return (0);
+}
