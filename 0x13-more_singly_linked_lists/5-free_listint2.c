7b1ca48d16d9a4fc6c4d3114b639f6525fbc3588
Daniel Ntlatleng <Daniel.Ntlatleng@gmail.com>
2022-11-05 03:53:26 -0700

    task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	while (*head != NULL)
+	if (head)
 	{
-		ptr = *head;
-		*head = (*head)->next;
-		free(ptr);
+		while (*head != NULL)
+		{
+			ptr = *head;
+			*head = (*head)->next;
+			free(ptr);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
